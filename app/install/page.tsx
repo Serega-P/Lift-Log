d4a637c9125666ad01201e8634a68c91c255dabc
Serega-P <serega.web.pro@gmail.com>
2025-02-26 12:18:19 +0000

    Установка приложения обновлена

--- app/install/page.tsx
+++ app/install/page.tsx
@@ -10,26 +10,10 @@ interface BeforeInstallPromptEvent extends Event {
   userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
 }
 
-// Расширяем `Navigator` для iOS
-interface NavigatorWithStandalone extends Navigator {
-  standalone?: boolean;
-}
-
 export default function InstallPage() {
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
-  const [isInstalled, setIsInstalled] = useState<boolean>(false);
 
   useEffect(() => {
-    // Проверяем, установлено ли PWA
-    const checkPWAInstalled = () => {
-      return (
-        window.matchMedia("(display-mode: standalone)").matches ||
-        Boolean((navigator as NavigatorWithStandalone).standalone)
-      );
-    };
-
-    setIsInstalled(checkPWAInstalled());
-
     // Ловим событие установки PWA
     const handleBeforeInstallPrompt = (event: Event) => {
       event.preventDefault();
@@ -50,11 +34,11 @@ export default function InstallPage() {
     await deferredPrompt.prompt();
     const { outcome } = await deferredPrompt.userChoice;
     if (outcome === "accepted") {
-      setIsInstalled(true);
+      // Установка прошла успешно, можно обновить интерфейс, если необходимо
     }
   };
 
-  return !isInstalled && deferredPrompt ? (
+  return (
     <div className="flex items-center justify-center flex-col px-10 w-full max-w-[420px] h-screen bg-slate-5">
       <Title
         text="Track progress effortlessly"
@@ -62,14 +46,14 @@ export default function InstallPage() {
         className="font-extrabold text-center leading-tight text-[42px]"
       />
       <Title text="Log workouts, monitor loads and improve results." size="sm" className="font-normal text-center" />
-      <Button onClick={handleInstall} className="bg-blue-500 rounded-lg mt-10" variant="accent" size="default">
-        <Download />
-        Install
-      </Button>
-    </div>
-  ) : (
-    <div className="flex items-center justify-center h-screen">
-      <Title text="App is already installed!" size="sm" className="font-extrabold" />
+      {deferredPrompt ? (
+        <Button onClick={handleInstall} className="bg-blue-500 rounded-lg mt-10" variant="accent" size="default">
+          <Download />
+          Install
+        </Button>
+      ) : (
+        <Title text="Install option not available" size="sm" className="font-normal text-center mt-10" />
+      )}
     </div>
   );
 }
